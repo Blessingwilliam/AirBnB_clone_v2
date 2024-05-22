908f9bfb700ec5c463a15af86c0b79135bc33ba0
Blessingwilliam <blessingwilliam195@gmail.com>
2024-05-22 21:06:08 +0000

    web_flask

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Script that starts a Flask web application"""
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -35,6 +35,18 @@ def python_is_cool(text="is cool"):
 def number(n):
     """Display n is a number only if n is an integer"""
     return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """Display an HTML page only if n is an integer"""
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """Display an HTML page only if n is an integer"""
+    return render_template('6-number_odd_or_even.html', n=n, parity="even" if n % 2 == 0 else "odd")
 
 
 if __name__ == "__main__":
